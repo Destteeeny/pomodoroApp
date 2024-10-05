3516f7f18208801888f867bccf574cc1e48e7ee9
Gonçalo Silva <111514781+Destteeeny@users.noreply.github.com>
2024-10-05 12:33:12 +0100

    Theme Options updated

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //depois de o doom carregar criamos o código dentro do evento.
 
     //SCRIPT DE THEMES
-    
     var selectHTML = document.getElementById('themes') // linha que identifica o <select> dentro do html
 
     // vai criar um evento sempre que detetar uma alteracao no selectHTML
@@ -13,21 +12,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         switch (theme) {
             case 'moon-theme':
-                document.body.style.backgroundImage = 'linear-gradient(to bottom, rgb(34, 0, 44),rgb(0, 5, 46))';
+                document.body.style.background = 'linear-gradient(to bottom, rgb(34, 0, 44), rgb(0, 5, 46))';
                 break;
             case 'lime-theme':
-                document.body.style.backgroundImage = 'linear-gradient(to bottom, rgb(24, 66, 49),rgb(68, 201, 101))';
+                document.body.style.background = 'linear-gradient(to bottom, rgb(24, 66, 49), rgb(68, 201, 101))';
                 break;
             case 'cherry-theme':
-                document.body.style.backgroundImage = 'linear-gradient(to bottom, rgb(71, 19, 19),rgb(199, 0, 60))';
+                document.body.style.background = 'linear-gradient(to bottom, rgb(71, 19, 19), rgb(199, 0, 60))';
                 break;
             case 'dark-theme':
-                document.body.style.backgroundImage = 'linear-gradient(to bottom, rgb(10, 10, 10),rgb(41, 41, 41))';
+                document.body.style.background = 'linear-gradient(to bottom, rgb(10, 10, 10), rgb(41, 41, 41))';
                 break;
             default:
-                document.body.style.backgroundImage = 'linear-gradient(to bottom, rgb(10, 10, 10),rgb(41, 41, 41))';
+                document.body.style.background = 'linear-gradient(to bottom, rgb(10, 10, 10), rgb(41, 41, 41))';
                 break;
         }
     })
 
+
+
+
+
+
+
+    //Contador
+    var TempoRestante = document.getElementById('tempo')
+
+    for(var i = 10; i>0; i--){
+        console.log(i)
+    }
 });
